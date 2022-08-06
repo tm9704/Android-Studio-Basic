2b4c92cf6d26c667acd5f194a437350ae91cfa13
tm9704 <tm9704@korea.ac.kr>
2022-08-06 15:38:06 +0900

    Android Studio : Counting Person App

--- app/src/main/java/com/tm9704/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/tm9704/myfirstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.tm9704.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener{
+            timesClicked += 1
+
+            tvMyTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey I'm a toast!", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
